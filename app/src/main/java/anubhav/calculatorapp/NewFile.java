a65cc606a4bd28a98c5e28a7480163ab1e52ed53
Maruf Hasan <marufhasan212@gmail.com>
2021-11-17 09:00:58 +0600

    new file is added

---
+++ app/src/main/java/anubhav/calculatorapp/NewFile.java
@@ -0,0 +1,23 @@
+package anubhav.calculatorapp;
+
+public class NewFile {
+    int a,b;
+    public NewFile() {
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
